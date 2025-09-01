67be2f08bc5857d30c2e1a9b9cc3fe3580288f50
GitNes26 <nestorpuentesin@gmail.com>
2025-09-01 00:41:36 -0600

    yssa,as

--- src/pages/Panel.jsx
+++ src/pages/Panel.jsx
@@ -54,29 +54,130 @@ export default function Panel() {
    }
 
    return (
-      <div className="h-screen p-6 bg-gray-900 text-white flex flex-col gap-3">
-         <h1 className="text-3xl font-bold mb-4">🕹️ Panel de Control</h1>
-         <div className="mb-2 flex items-center gap-4">
-            <span className="text-lg font-semibold">
-               Rondas jugadas: <span className="bg-yellow-500 text-black px-2 rounded">{rondasJugadas}</span>
-            </span>
+      <div className="h-screen w-full p-6 bg-gray-900 text-white gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+         {/* HEADER */}
+         <div className="card p-2 w-full bg-orange-500 ">
+            <div className="flex justify-between">
+               <h1 className="text-3xl font-bold">🕹️ Panel de Control</h1>
+               <button onClick={() => send({ action: "reset" })} className="btn  btn-info">
+                  Reset Juego
+               </button>
+            </div>
+
+            <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-4 p-4 rounded-lg text-black font-bold">
+               <div className="text-lg font-semibold">
+                  Rondas jugadas: <span className="bg-yellow-500 font-black text-black px-2 rounded">{rondasJugadas}</span>
+               </div>
+               <div className="text-lg font-semibold">
+                  Equipo Activo: <span className="bg-yellow-500 font-black text-black px-2 rounded">{equipoActivo ?? "-"}</span>
+               </div>
+               <div className="text-lg font-semibold">
+                  Equipo En Robo: <span className="bg-yellow-500 font-black text-black px-2 rounded">{enRobo ? "SI" : "NO"}</span>
+               </div>
+               <div className="text-lg font-semibold">
+                  Permitir Teclado: <input className="checkbox" type="checkbox" checked={true} onChange={() => {}} readOnly />
+               </div>
+            </div>
+         </div>
+
+         <div className="card p-4 w-full bg-gray-800 grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* VISTA PREVIA PREGUNTA */}
+            <div className="">
+               <h2 className="text-2xl font-bold mb-4">Vista Previa de la Pregunta</h2>
+               <div className="bg-gray-700 p-4 rounded-lg mb-4">
+                  <div className="mb-2 text-3xl font-semibold">{PREGUNTAS[preguntaPreview]?.texto ?? "..."}</div>
+                  <ul className="pl-4">
+                     {preguntaPreview
+                        ? PREGUNTAS[preguntaPreview]?.respuestas.map((r, i) => (
+                             <li
+                                key={i}
+                                className={`mb-5 text-3xl font-semibold transition-all ${
+                                   reveladas[`${preguntaPreview}-${i}`] === true ? "text-green-400 opacity-75" : "cursor-pointer hover:font-black"
+                                }`}
+                                onClick={() => (reveladas[`${preguntaPreview}-${i}`] === true ? null : destaparRespuesta(i))}
+                             >
+                                <span className="font-semibold">{i + 1}.</span> {r.texto} <span className="text-yellow-400 font-bold">{r.puntos}</span>
+                             </li>
+                          ))
+                        : Array.from({ length: 5 }).map((_, i) => (
+                             <li
+                                key={i}
+                                className={`mb-5 text-3xl font-semibold transition-all ${
+                                   reveladas[`${preguntaPreview}-${i}`] === true ? "text-green-400 opacity-75" : "cursor-pointer hover:font-black"
+                                }`}
+                                onClick={() => (reveladas[`${preguntaPreview}-${i}`] === true ? null : destaparRespuesta(i))}
+                             >
+                                <span className="font-semibold">{i + 1}.</span> {"....."} <span className="text-yellow-400 font-bold">{"..."}</span>
+                             </li>
+                          ))}
+                  </ul>
+                  <button
+                     className="btn btn-info mt-4 px-4 py-2 rounded font-bold"
+                     onClick={() => send({ action: "setQuestion", questionIdx: preguntaPreview })}
+                     disabled={preguntasEnviadas.includes(preguntaPreview)}
+                  >
+                     {preguntasEnviadas.includes(preguntaPreview) ? "Ya enviada" : "Enviar al tablero"}
+                  </button>
+               </div>
+            </div>
+            {/* CONTROLES */}
+            <div className="">
+               <h2 className="text-2xl font-bold mb-4">Controles</h2>
+
+               <div className="mt-4 flex gap-3">
+                  <div className="flex flex-col gap-2 justify-center items-center bg-gray-700 p-4 rounded-lg">
+                     <p className="font-medium text-2xl">Marcar Errores</p>
+                     <div className="flex gap-2">
+                        {Array.from({ length: 3 }).map((_, i) => (
+                           <button
+                              key={`btn-error-${i + 1}`}
+                              onClick={() =>
+                                 send({
+                                    action: "markError",
+                                    slot: i + 1
+                                 })
+                              }
+                              className={`btn btn-error btn-circle font-black btn-xl`}
+                              disabled={errores.e1 >= i + 1 || errores.e2 >= i + 1}
+                           >
+                              {i + 1}
+                           </button>
+                        ))}
+                     </div>
+                     {/* <button
+                        onClick={() =>
+                           send({
+                              action: "markError",
+                              slot: 2
+                           })
+                        }
+                        className={`btn btn-error`}
+                        disabled={errores.e1 >= 2 || errores.e2 >= 2}
+                     >
+                        Err 2
+                     </button>
+                     <button
+                        onClick={() =>
+                           send({
+                              action: "markError",
+                              slot: 3
+                           })
+                        }
+                        className={`btn btn-error`}
+                        disabled={errores.e1 >= 3 || errores.e2 >= 3}
+                     >
+                        Err 3
+                     </button> */}
+                  </div>
+                  <div className="flex flex-col gap-2 justify-center items-center bg-gray-700 p-4 rounded-lg">
+                     <p className="font-medium text-2xl">Activar Equipo</p>
+                     <div className="flex gap-2"></div>
+                  </div>
+               </div>
+            </div>
          </div>
 
          <div className="flex flex-col items-center justify-between mt-6  w-full bg-amber-500">
-            {/* ZONA DE RONDAS */}
-            {/* <div className="flex gap-2 justify-around w-full">
-               {Array.from({ length: 10 }).map((_, i) => (
-                  <button
-                     key={i}
-                     onClick={() => send({ action: "setQuestion", questionIdx: i })}
-                     className={`btn btn-neutral rounded-full shadow text-5xl font-black p-5 w-25 h-25 ${preguntaIdx === i ? "animate-pulse" : ""}`}
-                     disabled={i < preguntaIdx}
-                  >
-                     {i + 1}
-                  </button>
-               ))}
-            </div> */}
-
             <div className="flex w-full justify-between items-center">
                <div className="mt-4 flex gap-2">
                   <div className="flex gap-1">
@@ -116,15 +217,6 @@ export default function Panel() {
                      >
                         Err 3
                      </button>
-                  </div>
-
-                  <div className="flex ml-4 text-sm font-medium text-gray-600 justify-center items-center">
-                     <p>Equipo activo: {equipoActivo ?? "-"}</p>
-                     <p>En robo: {enRobo ? "SI" : "NO"}</p>
-                     <p className="mt-2">
-                        <label className="mr-2">Permitir teclado</label>
-                        <input type="checkbox" checked={true} onChange={() => {}} readOnly />
-                     </p>
                   </div>
                </div>
                <div className="flex gap-2 items-center">
@@ -166,46 +258,10 @@ export default function Panel() {
                         R/E
                      </button>
                   </div>
-                  <button
-                     onClick={() =>
-                        send({
-                           action: "reset"
-                        })
-                     }
-                     className="btn btn-accent"
-                  >
-                     Reset Juego
-                  </button>
                </div>
             </div>
          </div>
 
-         {preguntaPreview != null && (
-            <div className="bg-gray-700 p-4 rounded-lg mb-4">
-               <h2 className="text-xl font-bold mb-2">Vista previa de la pregunta</h2>
-               <div className="mb-2 font-semibold">{PREGUNTAS[preguntaPreview].texto}</div>
-               <ul className="pl-4">
-                  {PREGUNTAS[preguntaPreview].respuestas.map((r, i) => (
-                     <li
-                        key={i}
-                        className={`mb-5 text-3xl font-semibold transition-all ${
-                           reveladas[`${preguntaPreview}-${i}`] === true ? "text-green-400 opacity-75" : "cursor-pointer hover:font-black"
-                        }`}
-                        onClick={() => (reveladas[`${preguntaPreview}-${i}`] === true ? null : destaparRespuesta(i))}
-                     >
-                        <span className="font-semibold">{i + 1}.</span> {r.texto} <span className="text-yellow-400 font-bold">{r.puntos}</span>
-                     </li>
-                  ))}
-               </ul>
-               <button
-                  className="btn btn-info mt-4 px-4 py-2 rounded font-bold"
-                  onClick={() => send({ action: "setQuestion", questionIdx: preguntaPreview })}
-                  disabled={preguntasEnviadas.includes(preguntaPreview)}
-               >
-                  {preguntasEnviadas.includes(preguntaPreview) ? "Ya enviada" : "Enviar al tablero"}
-               </button>
-            </div>
-         )}
          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mb-6">
             {PREGUNTAS.map((pregunta, idx) => (
                <div
