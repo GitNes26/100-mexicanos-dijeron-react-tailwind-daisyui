a8d290d3f183fc07ba081e8abbbaac9f9806de5f
Nestor <nestorpuentesin@gmail.com>
2025-09-04 12:14:21 -0600

    se cambio de BrowserRouter a HashRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Tablero from "./pages/Tablero.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Panel from "./pages/Panel.jsx";
 import Control from "./pages/Control.jsx";
 import { JuegoContextProvider } from "./contexts/JuegoContext.jsx";
@@ -9,14 +9,14 @@ export default function App() {
    return (
       <div className="min-h-screen bg-slate-200 flex items-center justify-center">
          <JuegoContextProvider>
-            <BrowserRouter>
+            <HashRouter>
                <Routes>
                   <Route index path="/" element={<Tablero />} />
                   <Route index path="/tablero" element={<Tablero />} />
                   <Route path="/panel" element={<Panel />} />
                   <Route path="/control/:team" element={<Control />} />
                </Routes>
-            </BrowserRouter>
+            </HashRouter>
          </JuegoContextProvider>
       </div>
    );
